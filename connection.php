76d47f2443c6ffa3e10b986b11a395b541f8e4b9
MZ <nikita.zholkovskyi@gmail.com>
2020-10-26 10:20:02 +0100

    created insert.php

--- connection.php
+++ connection.php
@@ -3,3 +3,22 @@ declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+
+function openConnection(): PDO
+{
+    // No bugs in this function, just use the right credentials.
+    $dbhost = "localhost";
+    $dbuser = "becode";
+    $dbpass = "becode";
+    $db = "becode";
+
+    $driverOptions = [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
+}
+
+$pdo = openConnection();
